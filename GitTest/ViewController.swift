04da05e533baabeb9378b50b6fdacfaba1c9bf07
Tasit Sappooree <tasitsappooree@MacBook-Pro.local>
2021-12-14 11:51:23 +0700

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Tasit Sappooree on 14/12/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
