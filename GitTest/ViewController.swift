751d54d93dc16ac7d4a94bd25557b77bd288f605
Tasit Sappooree <jetdokoalah@gmail.com>
2021-12-14 15:05:55 +0700

    - Add label - Localization TH language

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let hello = NSLocalizedString("Hello", comment: "")
+        label.text = hello
     }
 
 
 }
-
